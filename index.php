93fb13344c50b458162cec3078ffb05fc002a5b9
anggunmelani <96399931+anggunmelani@users.noreply.github.com>
2021-12-21 10:59:09 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,157 @@
+<php>
+
+<!--bagian 1 head-->
+ <head>
+	<title>Pesona Purbalingga</title>
+	<link rel="shortcut icon" href="gambar/logo.png" type="image/x-icon">
+	<link rel="stylesheet" href="stylesheet.css">
+	<link rel="stylesheet" href="responsive.css">
+</head>
+<!--tutup bagian 1 head-->
+
+<!--bagian 2 atas-->
+<body>
+	<header>
+		<div class="container">
+		 <div class="header-left">
+		 	<img class="logo" src="gambar/won.png">
+		 </div>
+		 <div class="header-right">
+		 	<a href="#">Daerah</a>
+		 	<a href="#">Daftar</a>
+		 	<a href="#" class="login">Login</a>
+		  </div>
+		</div>
+	</header>
+<!--tutup bagian 2 atas-->
+
+<!--bagian 3 body-->
+	<div class="top-wrapper">
+	 <div class="container">
+	 	<div class="lesson-wrapper">
+	 	 <div class="lessons"><br>
+
+<!--gambar 1-->
+	<div class="lesson">
+	 <div class="lesson-icon">
+	   <img src="gambar/dlas.jpg" width="200px" heading="100px">
+	    <p>Lembah Asri</p>
+	  </div>
+	  <p class="text-contents">Lembah Asri merupakan salah satu wisata yang terletak di kaki gunung Slamet.</p>
+	</div>
+<!--tutup gambar 1-->
+
+<!--gambar 2-->
+	<div class="lesson">
+	 <div class="lesson-icon">
+	   <img src="gambar/owabong.jpg" width="200px" heading="100px">
+	    <p>Owabong</p>
+	  </div>
+	  <p class="text-contents">Wisata Owabong salah satu tempat rekreasi keluarga di Purbalingga yang memiliki wahana kolam renang.</p>
+	</div>
+<!--tutup gambar 2-->
+
+<!--gambar 3-->
+	<div class="lesson">
+	 <div class="lesson-icon">
+	   <img src="gambar/purbasari.jpg" width="200px" heading="100px">
+	    <p>Purbasari</p>
+	  </div>
+	  <p class="text-contents">Purbasari Pancuran Mas memrupakan Wahana yang memiliki bermacam jenis ikan. </p>
+	</div>
+<!--tutup gambar 3-->
+
+<!--gambar 4-->
+	<div class="lesson">
+	 <div class="lesson-icon">
+	   <img src="gambar/sanggaluri.jpg" width="200px" heading="100px">
+	    <p>Sanggaluri</p>
+	  </div>
+	  <p class="text-contents">Sanggaluri Park merupakan wahana taman pendidikan di Purbalingga.</p>
+	</div>
+<!--tutup gambar 4-->
+
+		<div class="clear"></div>
+	 </div>
+	</div>
+
+<!--bagian 4 tulisan-->
+	<h1>WELCOME TO</h1>
+	<h1>PURBALINGGA</h1>
+	<p>Kabupaten Purbalingga merupakan salah satu kabupaten yang berada di Provinsi Jawa Tengah</p>
+	 <div class="btn-wrapper">
+	 	<a href="#" class="btn signup">Share to Instagram</a>
+	 	<p>atau</p>
+	 	<a href="#" class="btn facebook"><span class="fa fa-facebook"></span>Share to Facebook</a>
+	 	<a href="#" class="btn twitter"><span class="fa fa-twitter"></span>Share to Twitter</a>
+	  </div>
+
+<!--tutup bagian 4 tulisan-->
+
+<!--bagian 5 tulisan dan gambar-->
+	<div class="lesson-wrapper">
+	 <div class="container">
+	  <div class="heading">
+	  	<h2>Cari tahu lebih lanjut tentang Purbalingga</h2>
+	  </div>
+	  <div class="lessons">
+	  	<div class="lesson">
+	  	 <div class="lesson-icon">
+	  	 	<img src="gambar/dlas.jpg" width="200px" heading="100px">
+	  	 	  <p>Lembah Asri</p>
+	  	 </div>
+	  	 <p class="text-contents">Lembah Asri merupakan salah satu wisata yang terletak di kaki gunung Slamet.</p>
+	  	</div>
+
+	  	<div class="lesson">
+	  	 <div class="lesson-icon">
+	  	 	<img src="gambar/owabong.jpg" width="200px" heading="100px">
+	  	 	  <p>Owabong</p>
+	  	 </div>
+	  	 <p class="text-contents">Wisata Owabong salah satu tempat rekreasi keluarga di Purbalingga yang memiliki wahana kolam renang.</p>
+	  	</div>
+
+	  	<div class="lesson">
+	  	 <div class="lesson-icon">
+	  	 	<img src="gambar/purbasari.jpg" width="200px" heading="100px">
+	  	 	  <p>Purbasari</p>
+	  	 </div>
+	  	 <p class="text-contents">Purbasari Pancuran Mas memrupakan Wahana yang memiliki bermacam jenis ikan. </p>
+	  	</div>
+
+	  	<div class="lesson">
+	  	 <div class="lesson-icon">
+	  	 	<img src="gambar/sanggaluri.jpg" width="200px" heading="100px">
+	  	 	  <p>Sanggaluri</p>
+	  	 </div>
+	  	 <p class="text-contents">Sanggaluri Park merupakan wahana taman pendidikan di Purbalingga.</p>
+	  	</div>
+
+	  	<div class="clear"></div>
+	 </div>
+	</div>
+<!--tuutp bagian 5 tulisan dan gambar-->
+
+<!-- bagian 6 message-->
+	<div class="message-wrapper">
+	 <div class="container">
+	  <div class="heading">
+	  	<h2> Selamat datang di Purbalingga</h2>
+	  	<h3>Kami siap membantu anda.Info lebih lanjut</h3>
+	  </div>
+	  <span class="btn-message">Klik disini</span>
+	 </div>
+	</div>
+<!--tutup bagian 6 message-->
+
+<!--bagian 7 footer-->
+	<footer>
+		<div class="container">
+		  <img src="gambar/won.png">
+		</div>
+		<p>Pesona Purbalingga</p>
+	</footer>
+<!--tutup bagian 7 footer-->
+
+ </body>
+</php>
